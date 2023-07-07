89a9b45e8cfc9b9e3eeb2c652acec6eaffff35ba
Vpolyushkevich@gmail.com <Vasily>
2023-07-07 12:22:10 +0300

    Add Task 28

--- Task 26.py
+++ Task 26.py
@@ -1,11 +1,11 @@
 """ Задача 26: Напишите программу, которая на вход принимает
 два числа A и B, и возводит число А в целую степень B с помощью рекурсии. """
-
-def exponention_a(a,b, c = 1):
-    c = a // c
+#
+def exponention_a(a, c, b ):
     if b != 1:
-        return exponention_a(c * a, b - 1, c)
+        return exponention_a(c * a, c,  b - 1)
     return a
 
 
-print(exponention_a(2,3))
+print(exponention_a(3, 3, 5))
+
